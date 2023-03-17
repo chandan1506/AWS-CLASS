b1cbdf4b2dd16cb839279780701cc963ae3ab1f0
chandan kumar <ck893607@gmail.com>
2023-03-17 17:02:13 +0530

    aws

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const app=express()
+app.use(express.json())
+
+
+app.get("/home",(req,res)=>{
+    res.send("welcome to the the server1")
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is listening on 3000")
+})
